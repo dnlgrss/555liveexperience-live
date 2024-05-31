1397bf843e2f057e6787b90e8aa81577bedd1d78
dnlgrss <dnldevs@gmail.com>
2024-05-31 18:21:54 +0200

    Vimeo muted on load to overcome chrome limitation

--- components/UI/VimeoVideo.jsx
+++ components/UI/VimeoVideo.jsx
@@ -61,10 +61,11 @@ const VimeoVideo = ({ horizontalVideoUrl, verticalVideoUrl, isAccordion = false,
         if (isHome) {
             return (
 
+
                 <div ref={videoRef} style={{ padding: '56.25% 0 0 0', position: 'relative', height: 'calc(100dvh - 65px)', margin: '0 auto', width: 'calc(100dvw - 32px)', objectFit: 'cover' }}>
                     {/* <div style={{ padding: '56.25% 0 0 0', position: 'relative', height: '100dvh', margin: '0 auto', width: 'calc(100dvw - 32px)' }}> */}
                     <iframe
-                        src={`${videoUrl}${autoplay}`}
+                        src={`${videoUrl}${autoplay}&muted=1&loop=1`}
                         // src={`${videoUrl}autoplay=1&loop=1&muted=0&controls=1&sidedock=0&title=0&byline=0&fullscreen=false&pip=0`}
                         style={{
                             position: 'absolute',
