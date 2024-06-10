68fb1611043f1356140720d566e895a619a882dd
dnlgrss <dnldevs@gmail.com>
2024-06-10 16:24:47 +0200

    Apollo cache implemented

--- helpers/apollo.js
+++ helpers/apollo.js
@@ -1,9 +1,21 @@
 // Add Apollo Client
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/client";
+
+const link = ApolloLink.from([
+    new HttpLink({
+        //Our graphql endpoint on wordpress
+        uri: `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/graphql`,
+        useGETForQueries: true
+    }),
+]);
 
 export const client = new ApolloClient({
-    //Our graphql endpoint on wordpress
-    uri: `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/graphql`,
+    link,
     // It allows our apollo app to cache requests in aplication memeory
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        query: {
+            fetchPolicy: 'network-only'
+        }
+    }
 })
