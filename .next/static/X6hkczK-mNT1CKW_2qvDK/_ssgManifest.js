fb1d31079e9b8ec4926fb0728c022d85c7edeadc
dnlgrss <dnldevs@gmail.com>
2024-11-01 13:48:22 +0100

    Fix .next

--- .next/static/X6hkczK-mNT1CKW_2qvDK/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcontacts","\u002Four-network","\u002Fwhy-us"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- .next/static/chunks/pages/version2-919ee5d94fa2b943.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[797],{5069:function(n,u,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/version2",function(){return i(9714)}])},9714:function(n,u,i){"use strict";i.r(u);var _=i(5893);i(7294),u.default=()=>(0,_.jsx)("div",{children:"Version2"})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5069)}),_N_E=n.O()}]);
